e5bff016de804b9ce948781d95a5a997cb50229d
Simon Altschuler <simon@altschuler.dk>
2014-05-22 14:34:37 +0200

    rename frontend to public, add d3 dep, move data and assets folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,33 @@
 var gulp = require('gulp'),
-    connect = require('gulp-connect');
+    connect = require('gulp-connect'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify');
+
+var public_root  = 'public';
+
+// dev
 
 gulp.task('connect', function() {
     connect.server({
-        root: '.',
+        root: public_root,
         livereload: true
     });
 });
 
 gulp.task('html', function () {
-    gulp.src('./*.html')
+    gulp.src(public_root + '/*.html')
         .pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
-    gulp.watch(['./**/*.{html,css,js}'], ['html']);
+    gulp.watch([public_root + '/**/*.{html,css,js}'], ['html']);
 });
 
+
 gulp.task('default', ['connect', 'watch']);
+
+// build
+
+gulp.task('copy', function () {
+
+});
