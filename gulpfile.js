8c63c11cfdb8219182f408fcb0b27b40a82cdb10
Simon Altschuler <simon@altschuler.dk>
2014-05-22 15:22:47 +0200

    fix gulp build and dev

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,43 @@
 var gulp = require('gulp'),
     connect = require('gulp-connect'),
     concat = require('gulp-concat'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    clean = require('gulp-clean');
 
-var public_root  = 'public';
+var public_root = 'public';
+var dist_root   = gulp.env.dist || 'dist';
 
 // dev
 
 gulp.task('connect', function() {
     connect.server({
-        root: public_root,
+        root: dist_root,
         livereload: true
     });
 });
 
-gulp.task('html', function () {
-    gulp.src(public_root + '/*.html')
+gulp.task('reload', ['build'], function () {
+    gulp.src(dist_root + '/*.html')
         .pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
-    gulp.watch([public_root + '/**/*.{html,css,js}'], ['html']);
+    gulp.watch([public_root + '/**/*.{html,css,js}'], ['reload']);
 });
-
 
 gulp.task('default', ['connect', 'watch']);
 
+
 // build
 
-gulp.task('copy', function () {
-
+gulp.task('clean', function () {
+    return gulp.src(dist_root)
+        .pipe(clean());
 });
+
+gulp.task('copy', ['clean'], function () {
+    return gulp.src(public_root + '/**')
+        .pipe(gulp.dest(dist_root));
+});
+
+gulp.task('build', ['copy']);
